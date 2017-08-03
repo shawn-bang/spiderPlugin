5755daae6c4fee63bb8a5d57b572f2c18e2c4ac5
Shawn <xiaobang6312@sina.com>
2017-08-03 16:42:29 +0800

    fix bug

--- src/main/java/com/credithc/spider/TemplateMain.java
+++ src/main/java/com/credithc/spider/TemplateMain.java
@@ -183,7 +183,7 @@ public class TemplateMain {
                 function.setFileNames("{" + "\"" + connection.getFileName() + "\"" + "}");
                 function.setFunctionName("do" + connection.getRequestMethod() + getFunctionName(connection.getFileName()));
                 function.setHeaders(connection.getRequestHeaders());
-                function.setHttpMethod("HTTP" + "." + connection.getRequestMethod());
+                function.setHttpMethod("HttpMethod" + "." + connection.getRequestMethod());
                 function.setUrl(connection.getRequestUrl());
                 function.setResultType(getReturnType(connection.getResponseHeaders().get("Content-Type")));
                 if (connection.isRequestBodyIsJson()){
